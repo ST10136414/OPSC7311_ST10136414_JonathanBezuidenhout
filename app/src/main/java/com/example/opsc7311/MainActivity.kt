1ac7fed2033a11aa10436c66ff4790417317d86d
DuMbLoSeR <102594859+DuMbLoSeR@users.noreply.github.com>
2024-04-29 15:26:23 +0200

    login screen

--- app/src/main/java/com/example/opsc7311/MainActivity.kt
+++ app/src/main/java/com/example/opsc7311/MainActivity.kt
@@ -6,21 +6,33 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         // Set padding using window insets
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-
-        }
+            insets*/
+        val button1 = findViewById<Button>(R.id.SignInToYourAccountBtn)
+        button1.setOnClickListener{}
+        val intent1 = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
+        val button2 = findViewById<Button>(R.id.SignUpForFreeBtn or R.id.noAccountBtn)
+        button1.setOnClickListener {}
+        val intent2 = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+        val button3 = findViewById<Button>(R.id.ForgotPasswordBtn)
+        button1.setOnClickListener {}
+        val intent3 = Intent(this, ForgotPasswordActivity::class.java)
+        startActivity(intent)
     }
+}
 
