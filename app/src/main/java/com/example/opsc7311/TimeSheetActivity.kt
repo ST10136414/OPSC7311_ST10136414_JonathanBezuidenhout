bd8aa7306ecf941f6e841ae875d86c0569868157
brice <ST10072411@vcconnect.edu.za>
2024-04-29 12:20:37 +0200

    5th change

--- app/src/main/java/com/example/opsc7311/TimeSheetActivity.kt
+++ app/src/main/java/com/example/opsc7311/TimeSheetActivity.kt
@@ -1,32 +1,39 @@
 package com.example.opsc7311
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.opsc7311.databinding.ActivityTimeSheetBinding
 
-class TimeSheetActivity : AppCompatActivity() {
+class TimeSheetActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_time_sheet)
+
+        val binding=ActivityTimeSheetBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //
+        binding.CalendarButton.setOnClickListener(this)
+
+
+        //no clue what this does????
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
 
-
-            /* What in the kentucky fried fuck
-            val spinner: Spinner = findViewById(R.id.user_spinner)
-        // Create an ArrayAdapter using the string array and a default spinner layout.
-            ArrayAdapter.createFromResource(this,R.array.user_array,android.R.layout.simple_spinner_item
-            ).also{ adapter ->
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                spinner.adapter = adapter
-            }*/
         }
+    }
+
+    override fun onClick(v: View?) {
+        //this navigates to the Calendar activity
+        openIntent(applicationContext, "parameter text", CalendarActivity::class.java)
     }
 }
