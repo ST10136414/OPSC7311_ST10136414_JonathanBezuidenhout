7a470c33159a6a475ae7248083ea2da91e9591d6
DuMbLoSeR <102594859+DuMbLoSeR@users.noreply.github.com>
2024-05-01 14:29:16 +0200

    all previous fixes on login screens

--- app/src/main/java/com/example/opsc7311/SignUpActivity.kt
+++ app/src/main/java/com/example/opsc7311/SignUpActivity.kt
@@ -1,6 +1,8 @@
 package com.example.opsc7311
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +12,11 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+            val button1 = findViewById<Button>(R.id.previousPageBtnSignUp)
+            button1.setOnClickListener {
+                val intent1 = Intent(this, MainActivity::class.java)
+                startActivity(intent1)
+            }
     }
 }
