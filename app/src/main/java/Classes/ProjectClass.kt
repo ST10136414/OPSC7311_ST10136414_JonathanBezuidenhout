1085443c19adfcdc279147950073a5a41bbdb0b6
Aiden <aidenbyrne53@gmail.com>
2024-05-01 16:32:17 +0100

    Added pages

---
+++ app/src/main/java/Classes/ProjectClass.kt
@@ -0,0 +1,14 @@
+package Classes
+
+import java.util.Date
+
+class ProjectClass
+{
+    lateinit var projectName:String
+    lateinit var projectColour:String
+    lateinit var clientName:String
+    lateinit var startDate: String
+    lateinit var endDate: String
+    lateinit var budget: String
+    lateinit var hourlyRate: String
+}
