9926a1ace867b4e58d69895283e663b9fafc186b
Aiden <aidenbyrne53@gmail.com>
2024-05-02 19:37:06 +0100

    ADd class

---
+++ app/src/main/java/Classes/EntryClass.kt
@@ -0,0 +1,10 @@
+package Classes
+
+class EntryClass
+{
+    lateinit var projectName:String
+    lateinit var note:String
+    lateinit var loggedTime:String
+    lateinit var plannedTime: String
+
+}
