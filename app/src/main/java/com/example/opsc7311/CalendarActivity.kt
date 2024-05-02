95c22a468f034bdc96f7e2c14837a663b1982e77
DuMbLoSeR <102594859+DuMbLoSeR@users.noreply.github.com>
2024-05-02 20:14:10 +0200

    i

--- app/src/main/java/com/example/opsc7311/CalendarActivity.kt
+++ app/src/main/java/com/example/opsc7311/CalendarActivity.kt
@@ -1,6 +1,8 @@
 package com.example.opsc7311
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -16,6 +18,24 @@ class CalendarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_calendar)
-
+        //nav bar elements
+        //Navigates to timesheet
+        val navTimShtBtn = findViewById<ImageView>(R.id.navBtnTimeSheet)
+        navTimShtBtn.setOnClickListener{
+            val timeSheetIntent = Intent(this, TimeSheetActivity::class.java)
+            startActivity(timeSheetIntent)
+        }
+        //navigates to timer page
+        val navTmrBtn = findViewById<ImageView>(R.id.navBtnTimer)
+        navTmrBtn.setOnClickListener{
+            val tmrIntent = Intent(this, TimerActivity::class.java)
+            startActivity(tmrIntent)
+        }
+        //navigates to More Options page
+        val navMreBtn = findViewById<ImageView>(R.id.navBtnMore)
+        navMreBtn.setOnClickListener {
+            val navMreIntent = Intent(this, MoreOptionsActivity::class.java)
+            startActivity(navMreIntent)
+        }
     }
 }
