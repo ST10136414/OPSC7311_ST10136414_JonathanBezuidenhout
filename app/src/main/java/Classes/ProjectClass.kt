ae51a14633c1bf9a2347c051bb24e0e75207f2a8
DuMbLoSeR <102594859+DuMbLoSeR@users.noreply.github.com>
2024-06-10 13:05:24 +0200

    yippee

--- app/src/main/java/Classes/ProjectClass.kt
+++ app/src/main/java/Classes/ProjectClass.kt
@@ -5,13 +5,13 @@ import java.util.Date
 class ProjectClass
 {
     lateinit var projectName:String
-    lateinit var projectColour:String
+    //lateinit var projectColour:String
     lateinit var clientName:String
     lateinit var startDate: String
     lateinit var endDate: String
     lateinit var budget: String
     lateinit var hourlyRate: String
-    lateinit var totaltime: String
+    //lateinit var totaltime: String
 
 
     companion object {
